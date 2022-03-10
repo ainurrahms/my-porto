fea0ff03ea9c325211d3b2df5abfe968fc053bad
ainurrahman <ainur.rahman@amartha.com>
2022-03-10 23:11:28 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="m-0 p-0">
+      <header>
+        <nav class="block text-center text-black w-full">
+          <ul class="mt-3 mb-3">
+            <li class="inline mr-24"><a class="font-light hover:font-bold" href="#home">Home</a></li>
+            <li class="inline mr-24"><a class="font-light hover:font-bold" href="#about">About Me</a></li>
+            <li class="inline mr-24"><a class="font-light hover:font-bold" href="#portofolio">Portofolio</a></li>
+            <li class="inline mr-24"><a class="font-light hover:font-bold" href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <div class="px-10">
+          <div class="flex w-full text-center min-h-screen">
+            <h1 className="font-mono text-3xl font-bold my-auto grow">
+                Hello,<br/>
+                Im Ainur Rahman
+            </h1>            
+          </div>
+        </div>
+        
       </header>
     </div>
   );
